e10609bf2259ab61ce011ab83c18a54414a55b86
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2020-07-18 13:09:24 -0400

    New reflection code

--- src/main/java/com/github/fernthedev/fernutils/reflection/ReflectionUtil.kt
+++ src/main/java/com/github/fernthedev/fernutils/reflection/ReflectionUtil.kt
@@ -1,104 +1,43 @@
-package com.github.fernthedev.fernutils.reflection;
+package com.github.fernthedev.fernutils.reflection
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-import org.apache.commons.lang3.EnumUtils;
-import org.jetbrains.annotations.Nullable;
-
-import java.text.NumberFormat;
-import java.text.ParseException;
-import java.util.Arrays;
-import java.util.List;
-
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class ReflectionUtil {
+import java.math.BigDecimal
 
 
+object ReflectionUtil {
+    private val classParser: MutableMap<Class<*>, (String) -> Any> =
+        HashMap()
 
-    public static final Class<?>[] validClassTypes = {
-            Short.class, short.class,
-            Float.class, float.class,
-            Integer.class, int.class,
-            Long.class, long.class,
-            Double.class, double.class,
-            Boolean.class, boolean.class,
-            Enum.class,
-            String.class,
-    };
+    init {
+        registerClassParser(Short::class.java) { s -> s.toShort()}
+        registerClassParser(Array<String>::class.java) { s -> s.split(" ").toTypedArray()}
+        registerClassParser(Float::class.java) { s -> s.toFloat()}
+        registerClassParser(Int::class.java) { s -> s.toInt()}
+        registerClassParser(Long::class.java) { s -> s.toLong()}
+        registerClassParser(Double::class.java) { s -> s.toDouble()}
+        registerClassParser(Boolean::class.java) { s -> s.toBoolean()}
+        registerClassParser(BigDecimal::class.java) { s -> s.toBigDecimal()}
+        registerClassParser(Byte::class.java) { s -> s.toByte()}
+    }
 
-    public static Object parseObject(Class<?> type, @Nullable Class<?>[] validClasses, String arg) {
-        if (validClasses == null || validClasses.length == 0) {
+    @JvmStatic
+    fun <T : Any> registerClassParser(
+        clazz: Class<T>,
+        parser: (String) -> T
+    ) {
+        classParser[clazz] = {s: String -> parser(s) } // Wrap to avoid type error
+    }
 
-            if (type.isEnum()) {
-                Class<? extends Enum<?>> enumClass = (Class<? extends Enum<?>>) type;
+    fun <T : Any> getClassParser(
+        clazz: Class<T>
+    ): (String) -> T {
+        return checkNotNull(classParser[clazz]) as (String) -> T
+    }
 
-                Object o = searchEnum(enumClass, arg);
-                if(o != null) {
-                    return o;
-                } else {
-                    throw new IllegalArgumentException("The argument " + arg + " is not valid for enum " + enumClass.getTypeName());
-                }
-
-            } else if (boolean.class.equals(type)) {
-                return Boolean.parseBoolean(arg);
-            } else if (int.class.equals(type)) {
-                return Integer.parseInt(arg);
-            } else if (long.class.equals(type)) {
-                return Long.parseLong(arg);
-            } else if (double.class.equals(type)) {
-                return Double.parseDouble(arg);
-            } else if (short.class.equals(type)) {
-                return Short.parseShort(arg);
-            } else if (String.class.equals(type)) {
-                return arg;
-            }
-        } else {
-
-            NumberFormat nf = NumberFormat.getInstance();
-            boolean isNumber = false;
-            try {
-                nf.parse(arg);
-                isNumber = true;
-            } catch (ParseException e) {}
-
-
-
-            for (Class<?> aClass : validClasses) {
-
-                if (aClass.isEnum()) {
-                    Class<? extends Enum> enumClass = (Class<? extends Enum>) aClass;
-                    if(EnumUtils.isValidEnum(enumClass, arg)) {
-                        return searchEnum(enumClass, arg);
-                    }
-                }
-
-                if(isNumber) {
-                    if (int.class.equals(aClass)) {
-                        return Integer.parseInt(arg);
-                    } else if (long.class.equals(aClass)) {
-                        return Long.parseLong(arg);
-                    } else if (double.class.equals(aClass)) {
-                        return Double.parseDouble(arg);
-                    } else if (short.class.equals(aClass)) {
-                        return Short.parseShort(arg);
-                    }
-                }
-
-                if ((arg.equalsIgnoreCase("true") || arg.equalsIgnoreCase("false")) && boolean.class.equals(aClass)) {
-                    return Boolean.parseBoolean(arg);
-                }
-
-
-
-                if(String.class.equals(aClass)) {
-                    return arg;
-                }
-            }
-
-            throw new IllegalArgumentException("The type could not be recognized: IsArgNumber: " + isNumber + " Classes Checked: " + Arrays.toString(validClasses) + " Value: " + arg);
-        }
-
-        throw new IllegalArgumentException("The type could not be recognized: " + type.getTypeName());
+    fun <T : Any> parse(
+        string: String,
+        clazz: Class<T>
+    ): T {
+        return checkNotNull(classParser[clazz])(string) as T
     }
 
     /**
@@ -107,25 +46,16 @@ public class ReflectionUtil {
      * @param arg
      * @return
      */
-    public static boolean isValid(List<Class<?>> classes, Object arg) {
-
-        for (Class<?> aClass : classes) {
-            if(aClass.isInstance(arg)) {
-                return true;
+    @JvmStatic
+    fun isValid(classes: List<Class<*>>, arg: Any?): Boolean {
+        for (aClass in classes) {
+            if (aClass.isInstance(arg)) {
+                return true
             }
         }
-
-        return false;
+        return false
     }
 
-    /**
-     * Checks if the argument provided is valid
-     * @param arg
-     * @return
-     */
-    public static boolean isValid(Object arg) {
-        return isValid(Arrays.asList(validClassTypes), arg);
-    }
 
     /**
      * Checks if the argument provided is valid
@@ -133,34 +63,26 @@ public class ReflectionUtil {
      * @param arg
      * @return
      */
-    public static boolean isValid(List<Class<?>> classes, Class<?> arg) {
-
-        for (Class<?> aClass : classes) {
-            if(aClass.equals(arg) || aClass.isAssignableFrom(arg)) {
-                return true;
+    @JvmStatic
+    fun isValid(classes: List<Class<*>>, arg: Class<*>): Boolean {
+        for (aClass in classes) {
+            if (aClass == arg || aClass.isAssignableFrom(arg)) {
+                return true
             }
         }
-
-        return false;
+        return false
     }
 
-    /**
-     * Checks if the argument provided is valid
-     * @param arg
-     * @return
-     */
-    public static boolean isValid(Class<?> arg) {
-        return isValid(Arrays.asList(validClassTypes), arg);
-    }
-
-    public static <T extends Enum<?>> T searchEnum(Class<T> enumeration,
-                                                   String search) {
-        for (T each : enumeration.getEnumConstants()) {
-            if (each.name().equalsIgnoreCase(search)) {
-                return each;
+    @JvmStatic
+    fun <T : Enum<*>?> searchEnum(
+        enumeration: Class<T>,
+        search: String?
+    ): T? {
+        for (each in enumeration.enumConstants) {
+            if (each!!.name.equals(search, ignoreCase = true)) {
+                return each
             }
         }
-        return null;
+        return null
     }
-
 }
